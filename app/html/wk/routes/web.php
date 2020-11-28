ad9ee7f73b31b8656c84e9e62f58a75513db2557
urushibata <urush1bata.lala@gmail.com>
2020-11-28 19:33:45 +0900

    laravel jetstream installed

--- app/html/wk/routes/web.php
+++ app/html/wk/routes/web.php
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia\Inertia::render('Dashboard');
+})->name('dashboard');
